21dba7bf8ca40f9ed232135d5755ec17c598935e
GZK-TXK <chocarrovaz.gaizka@gmail.com>
2026-04-07 11:16:59 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const ArrayVacio ) = [];
+
+const ArrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const NumerosPares = [2, 4, 6, 8];
+
+const ArrayBiDimiensional = [
+  [0, 1, 2],
+  ["a", "b", "c"],
+];
